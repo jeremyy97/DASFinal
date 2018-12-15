d042f104af2ae591bec705e28d62d842fbd27dc4
Antonio Josué Gaitán Quesada <agaitanq513@ulacit.ed.cr>
2018-12-15 10:17:38 -0600

    DBAccess

---
+++ Restaurant/ConsoleTestDelete/Program.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+using DBAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestDelete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User()
+
+        }
+    }
+}
---
+++ Restaurant/DBAccess/DBAccess.cs
@@ -0,0 +1,26 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBAccess
+{
+    public class DBAccess
+    {
+        static string connString = "Data Source=LAPTOP-BCKLLFPR;Initial Catalog=restaurantDB;Integrated Security=True;";
+
+        public List<User> GetUsers()
+        {
+            List<User> users;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                users = conn.Query<User>("Select * FROM USER").ToList();
+            }
+            return users;
+        }
+
+
+    }
+}
